2804cb695097a111cb0bd490d1e7b1a7f89c0a1e
MinaMHenry <mina.maurice32@gmail.com>
2021-08-28 03:21:51 +0200

    Adding Example of UnitTest

---
+++ TechnicalAssignment.Tests/Fixtures/DALFixture .cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TechnicalAssignment.DAL.Interfaces;
+using TechnicalAssignment.DAL.Repositories;
+
+namespace TechnicalAssignment.Tests.Fixtures
+{
+    public class DALFixture
+    {
+        public IUnitOfWork UnitOfWork { get; private set; }
+        public DALFixture()
+        {
+            SetupServices();
+            DataSetup();
+        }
+
+        private void SetupServices()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection.AddSingleton<IAccountRepository, AccountRepository>();
+            serviceCollection.AddSingleton<ICustomerRepository, CustomerRepository>();
+            serviceCollection.AddSingleton<ITransactionRepository, TransactionRepository>();
+            serviceCollection.AddSingleton<IUnitOfWork, UnitOfWork>();
+
+            var ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            UnitOfWork = ServiceProvider.GetService<IUnitOfWork>();
+        }
+        private void DataSetup()
+        {
+            UnitOfWork.CustomerRepository.CreatCustomer(new DAL.Models.Customer
+            {
+                CustomerID = 1,
+                Name = "Mina",
+                Surname = "Maurice"
+            });
+            UnitOfWork.CustomerRepository.CreatCustomer(new DAL.Models.Customer
+            {
+                CustomerID = 2,
+                Name = "John",
+                Surname = "Fekry"
+            });
+        }
+    }
+}
---
+++ TechnicalAssignment.Tests/Tests/DAL/CustomerRepositoryTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechnicalAssignment.DAL.Interfaces;
+using Xunit;
+using Microsoft.Extensions.DependencyInjection;
+using TechnicalAssignment.DAL.Models;
+using TechnicalAssignment.Tests.Fixtures;
+
+namespace TechnicalAssignment.Tests.Tests.DAL
+{
+    [Collection("DALFixture Collection")]
+    public class CustomerRepositoryTests
+    {
+        private readonly IUnitOfWork _sut;
+        public CustomerRepositoryTests(DALFixture Fixture)
+        {
+            _sut = Fixture.UnitOfWork;
+        }
+        [Fact]
+        public void GetCustomers()
+        {
+            var Customers = _sut.CustomerRepository.GetCustomers();
+            Assert.NotEmpty(Customers);
+        }
+        [Fact]
+        public void CreatCustomer()
+        {
+            bool result = _sut.CustomerRepository.CreatCustomer(new Customer()
+            {
+                CustomerID = 3,
+                Name = "Mina",
+                Surname = "Maurice"
+            });
+            Assert.True(result);
+            Customer cutomer = _sut.CustomerRepository.GetCustomer(3);
+            Assert.Equal(3, cutomer.CustomerID);
+        }
+        [Fact]
+        public void GetCustomer()
+        {
+            Customer cutomer = _sut.CustomerRepository.GetCustomer(1);
+            Assert.NotNull(cutomer);
+            Assert.Equal(1, cutomer.CustomerID);
+        }
+    }
+}
